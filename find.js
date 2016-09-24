b3e0b75e15b6878263cf932bec45b26ab43227f2
lee <lijianei.jj@gmail.com>
2016-09-24 09:53:08 +0800

    u

--- find.js
+++ find.js
@@ -1,38 +1,38 @@
-var http = require('http')
-var url = require('url')
-var queryString = require('querystring')
-var request = require('request')
 var cheerio = require("cheerio")
-var iconv = require("iconv-lite")
-http.createServer(function(req,resq){
-	
-	for(var i=0;i<=9;i++){
-		for(var j=0;j<=9;j++){	
-			var mobile = "153"+i+j+"135526"
-			var url = "http://www.ip138.com:8080/search.asp?mobile="+mobile+"&action=mobile"
-			var headers = {  
-				'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36'
-			}	
-			var options = {
-				url:url,
-				encoding:null,
-				headers:headers
-			}
-			resq.setHeader('Content-Type','text/html;charset=utf8')
-			request(options,function(e,res,body){
-				body=iconv.decode(body,"gb2312")
-				if(!e && res.statusCode==200){
-					var $=cheerio.load(body,{decodeEntities:false})
-					var address=$("table").eq(1).find("tr").eq(2).find(".tdc2").text().replace("&nbsp;","")
-					var mobile=$("table").eq(1).find("tr").eq(1).find(".tdc2").text().substring(0.11)
-					resq.write(mobile+"==>"+address+"<br>")
-				}else{
-					resq.write(e.message)
-				}
-				if(i==9&j==9)resq.end()
-			})
-		}
-	}
-}).listen(8080)
+var co = require('co')
+var corequest = require('co-request')
+var express = require('express')
+var app = express()
 
-console.log(" server reading ")
+app.use('/static',express.static('public'))
+app.get('/kingboo',function(req,res){
+	res.redirect("/static/kingboo.html")
+})
+app.get('/',function(req,resq){
+	
+	resq.set('Content-Type','text/html;charset=utf8')
+	resq.write("<style>*{margin:0;padding:0;}img{width:100%}</style>");
+	co(function*(){
+		for (var l = 1 ; l < 10 ; l++) {
+			var url = "http://m.show160.com/user/3887129/photo-p"+l
+			console.info(url)
+			var body = yield corequest(url)
+			var $=cheerio.load(body.body)//,{decodeEntities:false})
+			// console.info(body.body)
+			var rows=$('.rencai ul img')
+			
+			for(var i=0;i<rows.length;i++) {
+				var $e=rows.eq(i)
+				
+				resq.write(`<div><img src="${$e.attr('src').replace('_s.jpg','.jpg')}"></div>`)
+			}
+		}
+		resq.end()
+	})
+
+})
+var server = app.listen(80,function(){
+    var host = server.address().address,
+	port = server.address().port
+    console.log('app listening at http://%s:%s',host,port)
+})
